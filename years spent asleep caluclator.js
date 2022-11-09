5717114653c4ac436ea3f2da18695d484264349c
Jonatech256 <lutalo.jonathan@gmail.com>
2022-11-09 15:39:17 +0300

    turned the js code into function

--- years spent asleep caluclator.js
+++ years spent asleep caluclator.js
@@ -1,19 +1,30 @@
-//Average hrs slept per day//
-let ahspd = 0;
-//current age of user//
-let currentAge= 0;
-//Hours in 1 year //
-const hrsin1yr= 8760;
-//number of days in 1year //
-const daysin1yr = 365;
-// Hours slept in 1year//
-const hrsslptin1yr = ahspd * daysin1yr;
-// Total hours slept uptodate//
-const tthrsslpt = currentAge * hrsslptin1yr;
-// Total years spent sleeping //
-const ttyrsslpt = tthrsslpt / hrsin1yr;
-// total years spent awake//
-const ttyrsawake = currentAge - ttyrsslpt;
+// //Average hrs slept per day//
+// let ahspd = 5;
+// //current age of user//
+// let currentAge= 22;
+// //Hours in 1 year //
+// const hrsin1yr= 8760;
+// //number of days in 1year //
+// const daysin1yr = 365;
+// // Hours slept in 1year//
+// const hrsslptin1yr = ahspd * daysin1yr;
+// // Total hours slept uptodate//
+// const tthrsslpt = currentAge * hrsslptin1yr;
+// // Total years spent sleeping //
+// const ttyrsslpt = tthrsslpt / hrsin1yr;
+// // total years spent awake//
+// const ttyrsawake = currentAge - ttyrsslpt;
 
-console.log( " Years Spent Sleping :  " + ttyrsslpt + "Years");
-console.log( " Years Spent awake :  " + ttyrsawake + "Years");
+// console.log( " Years Spent Sleping :  " + ttyrsslpt + "Years");
+// console.log( " Years Spent awake :  " + ttyrsawake + "Years");
+
+function yearsSpentSleeping( age, avg){
+    const hrsin1yr = 8760; 
+    const daysin1yr = 365;
+    
+    const hrsslptin1yr = avg * daysin1yr;
+    const tthrsslpt = age * hrsslptin1yr;
+
+    return Math.round(tthrsslpt / hrsin1yr) 
+}
+console.log(yearsSpentSleeping(5,22));
